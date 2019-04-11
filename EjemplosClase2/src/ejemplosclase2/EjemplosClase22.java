753e386b3d9c2d5295d623e65f8d6b195ac91bf9
vysery98 <luispsi98@gmail.com>
2019-04-11 11:14:26 -0500

    cambio C22

---
+++ EjemplosClase2/src/ejemplosclase2/EjemplosClase22.java
@@ -0,0 +1,38 @@
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ * @author Fernando
+ */
+public class EjemplosClase22 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nombre,
+                apellido,
+                ciudad;
+
+        int edad;
+
+        System.out.println("Por favor, ingresar sus nombres: ");
+        nombre = scan.nextLine();
+
+        System.out.println("Por favor, ingresar sus apellidos: ");
+        apellido = scan.nextLine();
+
+        System.out.println("Por favor, ingresar su edad: ");
+        edad = scan.nextInt();
+        scan.nextLine(); // Limpieza del buffer de entrada de datos
+
+        System.out.println("Por favor, ingresar su ciudad: ");
+        ciudad = scan.nextLine();
+
+        System.out.println("Su nombre es: " + nombre + "\n\n\tSu apellido es: "
+                + apellido + ", edad:  " + edad + "\nCiudad: " + ciudad);
+
+    }
+
+}
