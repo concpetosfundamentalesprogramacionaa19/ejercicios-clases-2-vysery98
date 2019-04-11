47f9a7b516121763c9177898a7c73c001b52e53a
vysery98 <luispsi98@gmail.com>
2019-04-11 10:40:53 -0500

    ejemplosJava

---
+++ ejemplosclase2/EjemplosClase2.java
@@ -0,0 +1,28 @@
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ * @author Fernando
+ */
+public class EjemplosClase2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nombre,
+                apellido;
+
+        System.out.println("Por favor, ingresar sus nombres: ");
+        nombre = scan.next();
+
+        System.out.println("Por favor, ingresar sus apellidos: ");
+        apellido = scan.next();
+
+        System.out.println("Su nombre es: " + nombre + "\n\n\tSu apellido es: "
+                + apellido);
+
+    }
+
+}
---
+++ ejemplosclase2/EjemplosClase21.java
@@ -0,0 +1,27 @@
+package ejemplosclase2;
+
+import java.util.Scanner;
+
+/**
+ * @author Fernando
+ */
+public class EjemplosClase21 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nombre,
+                apellido;
+
+        System.out.println("Por favor, ingresar sus nombres: ");
+        nombre = scan.nextLine();
+        System.out.println("Por favor, ingresar sus apellidos: ");
+        apellido = scan.nextLine();
+
+        System.out.println("Su nombre es: " + nombre + "\n\n\tSu apellido es: "
+                + apellido);
+
+    }
+
+}
